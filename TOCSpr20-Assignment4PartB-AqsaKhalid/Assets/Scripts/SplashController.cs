2343c3e2851915e9aae0042ac926469dddce891f
sp19-rcs-009 <65023965+sp19-rcs-009@users.noreply.github.com>
2020-07-14 02:02:27 +0500

    done
    done

---
+++ TOCSpr20-Assignment4PartB-AqsaKhalid/Assets/Scripts/SplashController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+   public void GotoPenguinScene()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+
+    public void GotoHummingBirdScene()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+}
